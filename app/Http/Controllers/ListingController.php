dc4eb81c7df3aab81b873ef961dcf685662f7be2
Juan Pablo Balda <jpbalda@hotmail.com>
2018-01-18 21:54:06 -0500

    Chapter 4

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Listing;
+
+class ListingController extends Controller
+{
+    public function get_listing_api(Listing $listing)
+    {
+        $model = $listing->toArray();
+        for ($i = 1; $i <= 4; $i++) {
+            $model['image_' . $i] = asset('images/' . $listing->id . '/Image_' . $i . '.jpg');
+        }
+        return response()->json($model);
+    }
+}
